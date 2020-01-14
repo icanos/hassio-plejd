74ee9e0c2184f6d56756df8b7518e983102ce41a
icanos <marcus@sekurbit.se>
2020-01-14 17:28:41 +0000

    resolved container build error and upped version

--- plejd/main.js
+++ plejd/main.js
@@ -3,7 +3,7 @@ const mqtt = require('./mqtt');
 const fs = require('fs');
 const PlejdService = require('./ble');
 
-const version = "0.2.2";
+const version = "0.2.3";
 
 async function main() {
   console.log('starting Plejd add-on v. ' + version);

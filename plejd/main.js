b8f87d47a514702a55ab9b0191e4c8c565a019ae
icanos <marcus@sekurbit.se>
2020-02-29 16:06:51 +0000

    added missing file to dockerfile

--- plejd/main.js
+++ plejd/main.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const PlejdService = require('./ble.bluez');
 const SceneManager = require('./scene.manager');
 
-const version = "0.4.0";
+const version = "0.4.1";
 
 async function main() {
   console.log('starting Plejd add-on v. ' + version);

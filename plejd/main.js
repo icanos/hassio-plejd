3ba93d50e0b9359f59ba929d6e2cc93b379882ed
Marcus Westin <marcus@sekurbit.se>
2020-03-26 09:11:37 +0100

    fixed build error and upped version

--- plejd/main.js
+++ plejd/main.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const PlejdService = require('./ble.bluez');
 const SceneManager = require('./scene.manager');
 
-const version = "0.4.4";
+const version = "0.4.5";
 
 async function main() {
   console.log('starting Plejd add-on v. ' + version);

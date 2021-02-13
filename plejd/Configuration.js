a3244cd6fc9ce6f641d30a6a1f3b066cb5ae6629
Victor Hagelbäck <swevictor@gmail.com>
2021-02-13 12:58:18 +0100

    Read version info from config instead of hard-coded string

--- plejd/Configuration.js
+++ plejd/Configuration.js
@@ -2,26 +2,51 @@ const fs = require('fs');
 
 class Configuration {
   static _options = null;
+  static _addonInfo = null;
 
   static getOptions() {
     if (!Configuration._options) {
-      const rawData = fs.readFileSync('/data/options.json');
-      const config = JSON.parse(rawData);
-
-      const defaultRawData = fs.readFileSync('/plejd/config.json');
-      const defaultConfig = JSON.parse(defaultRawData).options;
-
-      Configuration._options = { ...defaultConfig, ...config };
-
-      // eslint-disable-next-line no-console
-      console.log('Config:', {
-        ...Configuration._options,
-        username: '---scrubbed---',
-        password: '---scrubbed---',
-        mqttPassword: '---scrubbed---',
-      });
+      Configuration._hydrateCache();
     }
     return Configuration._options;
+  }
+
+  static getAddonInfo() {
+    if (!Configuration._addonInfo) {
+      Configuration._hydrateCache();
+    }
+    return Configuration._addonInfo;
+  }
+
+  static _hydrateCache() {
+    const rawData = fs.readFileSync('/data/options.json');
+    const config = JSON.parse(rawData);
+
+    const defaultRawData = fs.readFileSync('/plejd/config.json');
+    const defaultConfig = JSON.parse(defaultRawData);
+
+    Configuration._options = { ...defaultConfig.options, ...config };
+    Configuration._addonInfo = {
+      name: defaultConfig.name,
+      version: defaultConfig.version,
+      slug: defaultConfig.slug,
+      description: defaultConfig.description,
+      url: defaultConfig.url,
+      arch: defaultConfig.arch,
+      startup: defaultConfig.startup,
+      boot: defaultConfig.boot,
+      host_network: defaultConfig.host_network,
+      host_dbus: defaultConfig.host_dbus,
+      apparmor: defaultConfig.apparmor,
+    };
+
+    // eslint-disable-next-line no-console
+    console.log('Config:', {
+      ...Configuration._options,
+      username: '---scrubbed---',
+      password: '---scrubbed---',
+      mqttPassword: '---scrubbed---',
+    });
   }
 }
 
--- plejd/main.js
+++ plejd/main.js
@@ -1,13 +1,17 @@
+const Configuration = require('./Configuration');
 const Logger = require('./Logger');
 const PlejdAddon = require('./PlejdAddon');
 
-const logger = Logger.getLogger('plejd-main');
-
-const version = '0.5.1';
-
 async function main() {
   try {
-    logger.info(`Starting Plejd add-on v. ${version}`);
+    // eslint-disable-next-line no-console
+    console.log('Starting Plejd addon and reading configuration...');
+
+    const addonInfo = Configuration.getAddonInfo();
+    const logger = Logger.getLogger('plejd-main');
+
+    logger.info(`Plejd add-on, version ${addonInfo.version}`);
+    logger.verbose(`Addon info: ${JSON.stringify(addonInfo)}`);
 
     const addon = new PlejdAddon();
 
@@ -15,7 +19,8 @@ async function main() {
 
     logger.info('main() finished');
   } catch (err) {
-    logger.error('Catastrophic error. Resetting entire addon in 1 minute', err);
+    // eslint-disable-next-line no-console
+    console.log('Catastrophic error. Resetting entire addon in 1 minute', err);
     setTimeout(() => main(), 60000);
   }
 }
